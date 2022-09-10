05ed0e5b18f9640b9906df46923ac5235756edcb
Eduardo Catalán <3026773130104@ingenieria.usac.edu.gt>
2022-09-09 20:48:51 -0600

    update

--- Modulos/cpu_201905554.c
+++ Modulos/cpu_201905554.c
@@ -49,7 +49,7 @@ static int escribir_archivo(struct seq_file *archivo, void *v)
 {   
     for_each_process(cpu){
         seq_printf(archivo,"PID,NOMBRE,USUARIO,ESTADO\n");
-        seq_printf(archivo, "%d,%s,%d,%d\n",cpu->pid,cpu->comm,cpu->cred->uid.val,cpu->__state);
+        seq_printf(archivo, "%d,%s,%d,%d\n",cpu->pid,cpu->comm,cpu->cred->uid.val,cpu->__state.val);
         seq_printf(archivo, "\n");
         list_for_each(lstProcess, &(cpu->children)){
             child = list_entry(lstProcess, struct task_struct, sibling);
